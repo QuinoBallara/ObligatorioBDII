b9593462267c5b9badec75b1397bd7eb6a5c29be
dutra1243 <facundodutraextra@gmail.com>
2025-06-25 19:13:54 -0300

    tipoEleccion endpoints implemented

---
+++ backend/src/services/tipoEleccionService.js
@@ -0,0 +1,52 @@
+const pool = require('../db/db').promise();
+
+async function getByID(id) {
+
+    const query = 'SELECT * FROM TipoEleccion WHERE id = ?';
+
+    try {
+        const [rows] = await pool.query(query, [id]);
+        if (rows.length > 0) {
+            return rows[0];
+        }
+        return null;
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+async function get() {
+
+    const query = 'SELECT * FROM TipoEleccion';
+
+    try {
+        const [rows] = await pool.query(query);
+        if (rows.length > 0) {
+            return rows;
+        }
+        return null;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function insert(nombre) {
+
+    const query = 'INSERT INTO TipoEleccion (nombre) VALUES (?)';
+     
+    try {
+        const [result] = await pool.query(query, [nombre]);
+        console.log('TipoEleccion inserted successfully:', result);
+        return { insertId: result.insertId };
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+module.exports = {
+    getByID,
+    get,
+    insert
+}
