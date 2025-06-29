06ea737abc0e8fcfb5ab07fb0c5b6f0fb8a868bc
Facundo3007 <facundodutraextra@gmail.com>
2025-06-29 03:42:47 -0300

    merge fix

--- frontend/src/pages/votacion/VotacionContent.jsx
+++ frontend/src/pages/votacion/VotacionContent.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import './votacion.css'
 import CardList from '../../components/UI/Votacion/CardList'
-import BlankVote from '../../components/UI/Votacion/BlankVote'
-import EpicSelector from '../../components/UI/Votacion/EpicSelector'
-import EndVote from '../../components/buttons/endVote'
 import RightContent from './RightContent'
 
 function VotacionContent() {
